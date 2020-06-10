2bae4e602720bd2c1fa423de9071399758a3fcb8
maofaming <229894116@qq.com>
2020-06-10 16:54:17 +0800

    更新代码

--- example/App.js
+++ example/App.js
@@ -62,13 +62,12 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <Text>test</Text>
-    // <NavigationContainer>
-    //   <Tab.Navigator>
-    //     <Tab.Screen name="Home" component={HomeScreen} />
-    //     <Tab.Screen name="Settings" component={SettingsScreen} />
-    //   </Tab.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
